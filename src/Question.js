fc0aa2fba918a31643860e278f7f71542e3414c9
Kenny Bates <kennyleebates@outlook.com>
2021-10-19 18:46:53 -0400

    Update Quetsion.js
    Created showInfo state to toggle info. If showInfo is true, show info prop. Additionally, added ternary operator to button so if showInfo is true -> use component with Minus, if showInfo is false use component icon Plus.

--- src/Question.js
+++ src/Question.js
@@ -2,13 +2,17 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'; // import react-icons
 
 const Questions = ({ title, info }) => {
+    const [showInfo, setShowInfo] = useState(false);
+    
     return (
         <article className='question'>
             <header>
                 <h4>{title}</h4>
-                <button className='btn'><AiOutlinePlus /></button>
+                <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                </button>
             </header>
-            <p>{info}</p>
+            {showInfo &&  <p>{info}</p>}
         </article>
     );
 };
