91ff968596b03edb344781ae9851ec8213c62874
Kenny Bates <kennyleebates@outlook.com>
2021-10-19 17:18:49 -0400

    Created Files
    Created files to setup project

---
+++ src/Question.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Questions = () => {
+    return(
+        <div>
+            <h2>Questions Component</h2>
+        </div>
+    );
+};
+
+export default Questions;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        id: 1,
+        title: 'Do I have to allow cookies?',
+        info: 'Cookies allow for a better experience on the web.',
+    },
+    {
+        id: 2,
+        title: 'How do I change my password?',
+        info: 'Click on the forgot my password link',
+    },
+    {
+        id: 3,
+        title: 'What is my UserID?',
+        info: 'Your userID is the ID you set to login as your username.',
+    },
+    {
+        id: 4,
+        title: 'How do I get free access?',
+        info: 'You can select the free trial at the bottom of the screen.',
+    },
+    {
+        id: 5,
+        title: 'How long does the free trial last?',
+        info: 'The free trial lasts for 30 days',
+    },
+];
+
+export default data;
